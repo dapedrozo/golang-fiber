1c92cc11f9618d7868a07fcf20ef80ed5e2e6ec7
dapedrozo <lanceroexperto@gmail.com>
2023-03-03 18:36:30 -0500

    commit project go with fiber

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id        string
+	Firstname string
+	Lastname  string
+}
+
+func handleUser(c *fiber.Ctx) error {
+	user := User{
+		Firstname: "John",
+		Lastname:  "Doe",
+	}
+	return c.Status(fiber.StatusOK).JSON(user)
+}
+
+func handleCreateUser(c *fiber.Ctx) error {
+	user := User{}
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+	user.Id = uuid.NewString()
+	return c.Status(fiber.StatusOK).JSON(user)
+}
+
+func main() {
+	app := fiber.New()
+	//middleware
+	app.Use(logger.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/about", func(c *fiber.Ctx) error {
+		return c.SendString("about")
+	})
+
+	app.Get("/empleo", func(c *fiber.Ctx) error {
+		return c.SendString("empleo")
+	})
+
+	app.Use(requestid.New())
+	userGroup := app.Group("/users")
+	userGroup.Get("", handleUser)
+	userGroup.Post("", handleCreateUser)
+
+	app.Listen(":3000")
+}
